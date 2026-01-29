d2922f1c23009100b7f3e10758ede6e7b9a0dbb9
Mossa <cgmossa@gmail.com>
2026-01-29 14:31:32 +0100

    bootstrap is set vendor crates

--- dvs-rpkg/bootstrap.R
+++ dvs-rpkg/bootstrap.R
@@ -1,7 +1,7 @@
 # bootstrap.R - Run before package build (Config/build/bootstrap: TRUE)
 
 if (.Platform$OS.type == "windows") {
-  system2("bash", c("-l", "-c", "./configure.win"))
+  system2("bash", c("-l", "-c", "./configure.win"), env = c(NOT_CRAN = "true"))
 } else {
-  system2("./configure")
+  system2("./configure", env = c(NOT_CRAN = "true"))
 }
