6a78a865e0419db62aa8e5582a9644df4d087a1f
Mossa <cgmossa@gmail.com>
2026-01-29 11:36:49 +0100

    ran `devtools::document("dvs-rpkg")`

--- dvs-rpkg/src/rust/lib.rs
+++ dvs-rpkg/src/rust/lib.rs
@@ -3,6 +3,6 @@
 //! This crate provides R bindings for the DVS (Data Version Control System).
 //! Results are returned as JSON strings for efficient parsing in R.
 
-miniextendr_module! {
+miniextendr_api::miniextendr_module! {
     mod dvs;
 }
